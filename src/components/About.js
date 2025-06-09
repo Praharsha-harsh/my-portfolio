779eda511d8b92285ef9ec6d46ae429aed7269bb
Praharsha-harsh <praharshahvp@gmail.com>
2025-06-09 15:21:19 +0530

    first

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import profile from '../assets/profile.jpg';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    {/* <img src={profile} alt="Profile" width="150" /> */}
+    <p>I am a React developer passionate about building web apps.</p>
+  </section>
+);
+
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => (
+  <section id="projects">
+    <h2>Projects</h2>
+    <div>
+      <h3>Portfolio Website</h3>
+      <p>Simple personal portfolio using React.</p>
+    </div>
+    {/* Add more projects as needed */}
+  </section>
+);
+
+export default Projects;
